68b35a8b36ed0b7b235837ffb8afd14160a10a17
Sefa Aktürk <102722821+akturksefa@users.noreply.github.com>
2023-01-08 21:38:54 +0300

    FCFS Editted

--- src/odev/FCFS.java
+++ src/odev/FCFS.java
@@ -8,7 +8,7 @@ import java.util.TreeSet;
 
 /**
  *
- * @author Hakan Cansu, Faruk Palta, Necati Babacan,Sefa Aktï¿½rk,ï¿½mer Cin
+ * @author Hakan Cansu, Faruk Palta, Necati Babacan,Sefa Aktürk,Ömer Cin
  */
 public class FCFS implements Comparator<Process> {
     
@@ -32,6 +32,18 @@ public class FCFS implements Comparator<Process> {
         }
     }
 
-    
+    @Override
+    public int compare(Process o1, Process o2) {
+        Process p1 = (Process)o1;
+        Process p2 = (Process)o2;
+        if (p1.getVarisZamani() < p2.getVarisZamani())
+            return (-1);
+ 
+        else if (p1.getVarisZamani() == p2.getVarisZamani() && p1.getOncelik() > p2.getOncelik())
+            return (-1);
+ 
+        else
+            return (1);
+    }
 
 }
