1a029ed95fceaf21ba767868a1d006553a470581
g201210079sakaryaedutr <83142386+g201210079sakaryaedutr@users.noreply.github.com>
2023-01-08 21:36:00 +0300

    FCFS Added

--- src/odev/RoundRobin.java
+++ src/odev/RoundRobin.java
@@ -8,7 +8,7 @@ import java.util.Timer;
 
 /**
  *
- * @author Hakan Cansu, Faruk Palta, Necati Babacan,Sefa Aktürk,Ömer Cin
+ * @author Hakan Cansu, Faruk Palta, Necati Babacan,Sefa Aktï¿½rk,ï¿½mer Cin
  */
 public class RoundRobin {
 
@@ -27,60 +27,12 @@ public class RoundRobin {
     private static final String PROSES_ZAMANASIMI = "proses zamanasimi";
 
     public static void main(String[] args) {
-        // TODO code application logic here
-
         
-       
-       long startTime = System.currentTimeMillis();
-        ReadAndWriteArray readAndWriteArray = new ReadAndWriteArray("giris.txt");
-        readAndWriteArray.outputArray(readAndWriteArray.getRobinArray());
-        
-        readAndWriteArray.prosesler.forEach(action ->{
-            System.out.println("ID : " + action.getProsesID() + ", Varis_Zamani : " + action.getVarisZamani() 
-                    +", Oncelik : " + action.getOncelik() + ", Proses_Zamani : " + action.getProsesZamani());
-        });
-        
-        Collections.sort(readAndWriteArray.prosesler, new FCFS());
-      
-        
-        seconds++;
-        
-        for (int i = 0; i < readAndWriteArray.prosesler.size(); i++) {
-            Timer timer = new Timer();
-            Process deger = readAndWriteArray.prosesler.get(i);
-            workingTime = (float) deger.getProsesZamani();
-            
-            
-            for (float j = 0; j < workingTime; j++) {
-                
-                if (j == 0) {
-                    System.out.printf("%.4f%s%s%s%04d%s%d%s%d%s\n", seconds, " sn ",PROSES_BASLADI, " (id:",
-                            deger.getProsesID(), " oncelik:", deger.getOncelik(),
-                            " kalan sure:", (int)(workingTime - j), " sn)");
-                }else if (j < workingTime) {
-                    System.out.printf("%.4f%s%s%s%04d%s%d%s%d%s\n", seconds, " sn ", PROSES_YURUTULUYOR, " (id:",
-                            deger.getProsesID(), " oncelik:", deger.getOncelik(),
-                            " kalan sure:", (int)(workingTime - j), " sn)");
-                    
-                }if (j == workingTime){
-                    System.out.printf("%.4f%s%s%s%04d%s%d%s%d%s\n", seconds, " sn ", PROSES_SONLANDI, " (id:", 
-                            deger.getProsesID(), "oncelik:", deger.getOncelik(),
-                            "kalan sure", (int)(workingTime - j), " sn)");
-                    
-                }
-            }
-            try {
-                    Thread.sleep(1000);
-                    
-                    
-                } catch (Exception e) {
-                }
-                seconds++;
         }
     }
     
 
     
     
-}
+
 
